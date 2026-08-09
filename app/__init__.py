16f59a2cc389ac5f712b9625014c789d3fad697c
itsdaudi <daudikombe77@gmail.com>
2026-08-09 19:20:00 +0300

    feat adding of files

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from app.config import Config
+from app.extensions import db, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route("/")
+    def home():
+        return {
+            "message": "Welcome to MatchPulse API",
+            "status": "running"
+        }
+
+    return app
+
+
+    
